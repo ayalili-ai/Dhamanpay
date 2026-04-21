43f07ad9a96d1163c2bd5fe3303f778aa815e866
bourzak-aya-lilia <ayalili764@gmail.com>
2026-04-21 01:23:40 +0100

    stuff with render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,12 +10,9 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     public function register(): void
-{
-    $this->app->singleton('db.connector.pgsql', function () {
-        return new \App\Database\Connectors\NeonPostgresConnector();
-    });
-}
-
+    {
+        //
+    }
 
     /**
      * Bootstrap any application services.
